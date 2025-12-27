6770972058d064ee71bbfa0b2402d796a6461a15
qq1660396635 <1660396635@qq.com>
2025-12-27 08:18:21 +0800

    Delete B. 导航逻辑 directory

--- B. 导航逻辑/2.信息聚合/5_破解配置config.js
+++
@@ -1,8 +0,0 @@
-const sites = [
-    {name: '笔趣阁', url: 'https://m.bqg128.cc/', icon: '📚'},
-    {name: '读之阁', url: 'http://sgm.duzhige.net/', icon: '📖'},
-    {name: '娜娜', url: 'https://nana00.com/xs124595/', icon: '📝'},
-    {name: '风车动漫', url: 'https://www.dm304.com/', icon: '🎬'},
-    {name: '王者TV', url: 'https://wzzy.cc/', icon: '👑'},
-    {name: '漫画', url: 'https://m.kuaikanmanhua.com/mobile/2583/list/?module_id=0&topic_id=2583&module_title&module_type', icon: '🎨'}
-];
